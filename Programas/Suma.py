8fc6597cca46a25e89f25c0cb593cf2cbc696aea
JECR2005 <L23070410@cdmadero.tecnm.mx>
2025-10-06 14:44:21 -0600

    first commit

---
+++ Programas/Suma.py
@@ -0,0 +1,12 @@
+# Programa para sumar dos números
+
+# Pedir los números al usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Calcular la suma
+suma = num1 + num2
+
+# Mostrar el resultado
+print("La suma es:", suma)
+
